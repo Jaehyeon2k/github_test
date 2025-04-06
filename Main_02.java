8e42721337fd07c868c28905dbaae9e846ff69f9
jaehyeon <wmspfl2673@gmail.com>
2025-04-07 00:58:02 +0900

    행렬 덧셈

---
+++ Main_02.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main_02 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int N = scn.nextInt();
+        int M = scn.nextInt();
+
+        int[][] A = new int[N][M];
+        int[][] B = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                A[i][j] = scn.nextInt();
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                B[i][j] = scn.nextInt();
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                System.out.print(A[i][j] + B[i][j] + " ");
+            }
+            System.out.println(); 
+        }
+
+        scn.close();
+    }
+}
